b8d2d6f0a113e95f599dc2be8df6616b602ddb52
Amangastore <aybelsitr@gmail.com>
2025-08-14 15:53:13 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
-
 (function(){
   const page = (location.pathname.split('/').pop() || 'index.html').toLowerCase();
   document.querySelectorAll('.navlinks a').forEach(a=>{
-    const h = a.getAttribute('href').toLowerCase();
-    if ((h==='./' && (page===''||page==='index.html')) || h===page) a.classList.add('active');
+    const href = a.getAttribute('href').toLowerCase();
+    if (href === page || (page === '' && href === 'index.html') || (page==='index.html' && (href==='index.html'||href==='./'))) {
+      a.classList.add('active');
+    }
   });
 })();
